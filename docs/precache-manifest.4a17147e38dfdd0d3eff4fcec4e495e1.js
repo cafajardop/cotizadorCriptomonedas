607293c8cde05f38e889ccee52e20a98d73a996b
carlos andres fajardo pedraza <carlosafp84@hotmail.com>
2020-10-15 22:21:13 -0500

    carga docs

---
+++ docs/precache-manifest.4a17147e38dfdd0d3eff4fcec4e495e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a08757554f93b3216ce9ef498cfb05a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c31b42ffb135baa1562",
+    "url": "/static/css/main.fdca9178.chunk.css"
+  },
+  {
+    "revision": "6bfb8a2aa87fecec1031",
+    "url": "/static/js/2.c30ba0c8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.c30ba0c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c31b42ffb135baa1562",
+    "url": "/static/js/main.373d6ce8.chunk.js"
+  },
+  {
+    "revision": "27343cb691de655e28f8",
+    "url": "/static/js/runtime-main.ba78b093.js"
+  },
+  {
+    "revision": "96e45d694833f161c9dc02ab9dde4a6b",
+    "url": "/static/media/cryptomonedas.96e45d69.png"
+  }
+]);
