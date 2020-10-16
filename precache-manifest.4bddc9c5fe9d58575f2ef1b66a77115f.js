2adc4199dc3463317330090f4be435c282078e88
carlos andres fajardo pedraza <carlosafp84@hotmail.com>
2020-10-15 22:30:10 -0500

    Updates

---
+++ precache-manifest.4bddc9c5fe9d58575f2ef1b66a77115f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87a2f92ceaebf00f77cc65f266713765",
+    "url": "/cotizadorCriptomonedas/index.html"
+  },
+  {
+    "revision": "c511d7ee9e96b0537caa",
+    "url": "/cotizadorCriptomonedas/static/css/main.fdca9178.chunk.css"
+  },
+  {
+    "revision": "6bfb8a2aa87fecec1031",
+    "url": "/cotizadorCriptomonedas/static/js/2.c30ba0c8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/cotizadorCriptomonedas/static/js/2.c30ba0c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c511d7ee9e96b0537caa",
+    "url": "/cotizadorCriptomonedas/static/js/main.5720a66b.chunk.js"
+  },
+  {
+    "revision": "d23335f2ba52ec83b5c4",
+    "url": "/cotizadorCriptomonedas/static/js/runtime-main.dab0b8ef.js"
+  },
+  {
+    "revision": "96e45d694833f161c9dc02ab9dde4a6b",
+    "url": "/cotizadorCriptomonedas/static/media/cryptomonedas.96e45d69.png"
+  }
+]);
